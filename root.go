94bbe0a21de5ea3f67015acca211fd00737ce568
Azim Sonawalla <azim@liveramp.com>
2019-08-05 13:44:27 -0400

    cobra initialization

---
+++ root.go
@@ -0,0 +1,98 @@
+/*
+Copyright © 2019 Azim Sonawalla <azim.sonawalla@gmail.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package main
+
+import (
+	"fmt"
+	"os"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "unbake",
+	Short: "Convert docker buildx bake files into sequential docker commands",
+	Long: `docker/buildx is a plugin to the most recent docker distribution
+that allows users to define high level build information (bake files)
+and transmit that build info to the buildkit-powered docker daemon
+in a single invocation.
+
+Since this is a relatively new piece of technolgoy, many environments (including
+primarily CI and CD systems) don't yet have the most recent version of docker or
+the buildx plugin, and it introduces significant overhead to install these.
+
+unbake solves for this discrepancy by taking a bake file as an input and generating
+plain docker commands to build those targets. You'd want to run the unbake container
+in your CI against a bake file and pipe the output of it to shell to continue with a
+multi-invocation docker build process.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	//	Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.unbake.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// Search config in home directory with name ".unbake" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".unbake")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
